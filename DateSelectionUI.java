21b0c6fd1c66cd5f1ff572ff461ae6fa8a18cffe
your name <your github email>
2023-01-13 19:31:54 +0800

    added headerPanel with headerLabel in DateSelectionUI

--- DateSelectionUI.java
+++ DateSelectionUI.java
@@ -4,7 +4,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DateSelectionUI {
-    JFrame f;
+    //JFrame
+    static JFrame f;
+
+    //JPanel
+    static JPanel headerPanel;
+
+
+    //JLabel
+    static JLabel headerLabel;
+
+    public static void buildHeaderPanel()
+    {
+        headerPanel = new JPanel();
+        headerPanel.setBounds(0,0, 700, 75);
+        headerPanel.setBackground(new Color(54, 33, 89));
+        headerPanel.setLayout(new GridBagLayout());
+
+        //Label for header
+        headerLabel = new JLabel("Select Date");
+        headerLabel.setFont(new Font(Font.DIALOG, Font.ITALIC, 50));
+        headerLabel.setForeground(Color.white);
+
+        headerPanel.add(headerLabel);
+        f.add(headerPanel);
+    }
+
     DateSelectionUI()
     {
         //set properties of f
@@ -15,5 +40,9 @@ public class DateSelectionUI {
         f.setResizable(false);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
         f.setLocationRelativeTo(null);
+
+
+        buildHeaderPanel();
+
     }
 }
