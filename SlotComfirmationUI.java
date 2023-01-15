5a3b272bfe5157c11603b735c10afd851ef16627
your name <your github email>
2023-01-15 10:06:50 +0800

    added buildInstructionPanel function to build instruction panel with labels in SlotComfirmationUI class

--- SlotComfirmationUI.java
+++ SlotComfirmationUI.java
@@ -1,11 +1,39 @@
 package hospitalgui;
 
 import javax.swing.*;
+import java.awt.*;
+
 
 public class SlotComfirmationUI {
+    //JFrame
     static JFrame f;
+
+
+    //JPanels
+    static JPanel instructionPanel;
+
+
+    //JLabels
+    static JLabel instructionLabel;
+
+
+    public static void buildInstructionPanel()
+    {
+        instructionPanel = new JPanel();
+        instructionPanel.setBounds(0, 0, 700, 50);
+        instructionPanel.setLayout(new GridBagLayout());
+        instructionPanel.setBackground(Color.pink);
+
+        instructionLabel = new JLabel("Select appointment duration and ensure appointment details");
+        instructionLabel.setFont(new Font(Font.DIALOG, Font.BOLD, 20));
+        instructionPanel.add(instructionLabel);
+        f.add(instructionPanel);
+    }
+
+    
     SlotComfirmationUI(int i , int j)
     {
+        //initialize and set properties of f
         f = new JFrame("Slot confirm");
         f.setSize(700,500);
         f.setLayout(null);
@@ -13,5 +41,7 @@ public class SlotComfirmationUI {
         f.setResizable(false);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
         f.setLocationRelativeTo(null);
+
+        buildInstructionPanel();
     }
 }
