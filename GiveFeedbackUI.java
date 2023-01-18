d4ea76847a285fd7976c81a0d30f1139b3b2ce50
Jiunn Ming <keejiunnming4896@gmail.com>
2023-01-18 14:53:58 +0800

    added buildHeaderPanel function to added header panel to GiveFeedbackUI class

--- GiveFeedbackUI.java
+++ GiveFeedbackUI.java
@@ -1,12 +1,39 @@
 package hospitalgui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GiveFeedbackUI {
+    //Jframe
     JFrame f;
+
+
+    //JPanel
+    JPanel headerPanel;
+
+    //JLabel
+    JLabel headerLabel;
+
+    public void buildHeaderPanel()
+    {
+        headerPanel = new JPanel();
+        headerPanel.setBounds(0, 0, 1280, 100);
+        headerPanel.setBackground(new Color(236, 181, 181));
+        headerPanel.setLayout(new GridBagLayout());
+        headerLabel = new JLabel("Give Feedback");
+        headerLabel.setFont(new Font(Font.DIALOG, Font.BOLD, 35));
+        headerPanel.add(headerLabel);
+        f.add(headerPanel);
+    }
+
+
     public GiveFeedbackUI()
     {
         f = new JFrame("Give Feedback");
+
+        buildHeaderPanel();
+
+
 
         f.setSize(1280,720);
         f.setLayout(null);
